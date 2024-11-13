def7a5624d508f7c768c192a9467596d752a3c64
Rheyya Hollins <hollinsrheyya@gmail.com>
2024-11-13 00:30:14 -0500

    finished

---
+++ Movie Database/Movie Database/Movie.cs
@@ -0,0 +1,14 @@
+﻿
+    internal class Movie
+    {
+        public string movieTitle;
+        public string movieCatergory;
+
+        public Movie(string title, string catergory)
+        {
+            movieTitle = title;
+            movieCatergory = catergory;
+        }
+        
+    }
+
--- Movie Database/Movie Database/Program.cs
+++ Movie Database/Movie Database/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+List<Movie> movies = new List<Movie>()
+{
+    new Movie ("Monster Ink", "Animation"),
+    new Movie ("The Notebook", "Romantic"),
+    new Movie ("Bird Box", "Thriller"),
+    new Movie ("Scream", "Thriller"),
+    new Movie ("Bridesmaids", "Comedy"),
+    new Movie ("8 Mile", "Drama"),
+    new Movie ("The Godfather", "Crime"),
+    new Movie ("The Matrix", "Sci-Fi"),
+    new Movie ("Spider-Man", "Action"),
+    new Movie ("Spongebob Movie", "Animation")
+
+};
+
+Console.WriteLine("Welcome to the Movie List Application!");
+Console.WriteLine($"There are {movies.Count} movies in this list.");
+
+bool continueSearching = true;
+while (continueSearching)
+{
+    Console.WriteLine("What category are you interested in?");
+    string userCategory = Console.ReadLine().ToLower();
+
+    var matchingMovies = movies.Where(x => x.movieCatergory.ToLower() == userCategory).ToList();
+
+    if (matchingMovies.Any())
+    {
+       
+        matchingMovies.ForEach(movie => Console.WriteLine(movie.movieTitle));
+    }
+    else
+    {
+        Console.WriteLine($"No movies found in the category {userCategory}");
+    }
+
+    Console.Write("Continue? (y/n): ");
+    continueSearching = Console.ReadLine().ToLower() == "y";
+
+   // Console.WriteLine("Thank you for using the Movie List Application!");
+
+}
+Console.WriteLine("Thank you for using the Movie List Application!");
